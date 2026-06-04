756390a0191b647c2487486d14cba7504fa1f37a
jaumrosa <joao.rosa5@outlook.com>
2026-06-04 04:40:21 -0300

    MY18 - Criação de módulo admin com getMenus e renderização dinâmica do menu lateral com ícones e estado ativo

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const users = require('../inc/users');
+const admin = require('../inc/admin');
 const router = express.Router();
 
 router.use(function(req, res, next){
@@ -10,13 +11,20 @@ router.use(function(req, res, next){
     }
 });
 
+router.use(function(req, res, next){
+    req.menus = admin.getMenus();
+    next()
+});
+
 router.get("/logout", function(req, res, next){
     delete req.session.user;
     res.redirect("/admin/login");
 });
 
 router.get("/", function(req, res, next){
-    res.render("admin/index");
+    res.render("admin/index", {
+        menus: req.menus
+    });
 });
 
 router.post("/login", function(req, res, next){
@@ -39,25 +47,34 @@ router.get("/login", function(req, res, next){
 });
 
 router.get("/contacts", function(req, res, next){
-    res.render("admin/contacts");
+    res.render("admin/contacts", {
+        menus: req.menus
+    });
 });
 
 router.get("/emails", function(req, res, next){
-    res.render("admin/emails");
+    res.render("admin/emails", {
+        menus: req.menus
+    });
 });
 
 router.get("/menus", function(req, res, next){
-    res.render("admin/menus");
+    res.render("admin/menus", {
+        menus: req.menus
+    });
 });
 
 router.get("/reservations", function(req, res, next){
     res.render("admin/reservations", {
-        date: {}
+        date: {},
+        menus: req.menus
     });
 });
 
 router.get("/users", function(req, res, next){
-    res.render("admin/users");
+    res.render("admin/users", {
+        menus: req.menus
+    });
 });
 
 module.exports = router;
