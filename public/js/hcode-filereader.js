7b89ddd52fec119899c3267c4c32bd03e78bc705
João Pedro Rosa <joao.rosa5@outlook.com>
2026-06-07 23:32:34 -0300

    MY26 - Criação da classe HcodeFileReader para preview de imagem via FileReader antes do upload

---
+++ public/js/hcode-filereader.js
@@ -0,0 +1,32 @@
+class HcodeFileReader {
+
+    constructor(inputEl, imgEl){
+        this.inputEl = inputEl;
+        this.imgEl = imgEl;
+
+        this.initInputEvent();
+    }
+
+    initInputEvent(){
+        document.querySelector(this.inputEl).addEventListener("change", e => {
+            this.reader(e.target.files[0]).then(result => {
+                document.querySelector(this.imgEl).src = result;
+            });
+        });
+    }
+
+    reader(file){
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                resolve(reader.result);
+            }
+
+            reader.onerror = () => {
+                reject("Não foi possível ler a imagem")
+            }
+            reader.readAsDataURL(file);
+        });
+        
+    }
+}
