d9690282bcac263673ba87e4ef50c25cf3e9eb42
João Pedro Rosa <joao.rosa5@outlook.com>
2026-06-08 01:21:06 -0300

    MY28 - Refatoração do save para suportar INSERT e UPDATE com foto opcional e resposta JSON padronizada

--- public/js/hcode-formsave.js
+++ public/js/hcode-formsave.js
@@ -1,22 +1,26 @@
-HTMLFormElement.prototype.save = function (){
+HTMLFormElement.prototype.save = function() {
     const form = this;
-    return new Promise((resolve, reject) => {
-        form.addEventListener('submit', e => {
-            e.preventDefault();
-            const formCreateData = new FormData(formCreate);
-            
-            fetch(form.action, {
-                method: form.method,
-                body: formCreateData
-            })
-            .then(response => response.json())
-            .then(json => {
-                resolve(json);
-            })
-            .catch(err => {
-                reject(err);
-            });
-        });
-    })
     
-}
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        
+        const formData = new FormData(form);
+        
+        fetch(form.action, {
+            method: form.method,
+            body: formData
+        })
+        .then(response => response.json())
+        .then(json => {
+            if (json.success) {
+                window.location.reload();
+            } else {
+                alert(json.error);
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert('Erro ao processar requisição, verifique no console');
+        });
+    });
+};
